cfe8bb9b3ab14a2b2bad858f135ce5183b5372ad
Ahmed <Ahmed.bahloul00@eng-st.cu.edu.eg>
2022-05-29 04:06:10 +0200

    added quad struct

--- Source Code/quadruple.h
+++ Source Code/quadruple.h
@@ -1,5 +1,5 @@
 #include "dataTypes.h"
-#include "string.h"
+#include <string.h>
 #include <stdio.h>
 
 struct quadruple_info
@@ -7,20 +7,40 @@ struct quadruple_info
   char *opcode;
   char *operand1;
   char *operand2;
-  char *result;
+  int result;
 };
 // define stack
 struct Stack
 {
   int top;
-  char *items[100];
+  char* items[100];
+  int id_quadruple; // t1,t2 
 };
+
+char* pop(struct Stack **quad_stack){
+  return (*quad_stack)->items[--((*quad_stack)->top)];
+}
 
 void push(struct Stack *quad_stack, char *str)
 {
-  quad_stack->top++;
   quad_stack->items[quad_stack->top] = (char *)malloc(strlen(str) + 1);
   strcpy(quad_stack->items[quad_stack->top], str);
+  quad_stack->top++;
+  if(quad_stack->top == 3){
+    // pop stack and print 
+    char temp[4]; 
+    itoa(quad_stack->id_quadruple,temp,10);
+    struct quadruple_info* quad_info = (struct quadruple_info *)malloc(sizeof(struct quadruple_info));
+    quad_info->opcode = pop(&quad_stack);
+    quad_info->operand2 = pop(&quad_stack);
+    quad_info->operand1 = pop(&quad_stack);
+    quad_info->result = quad_stack->id_quadruple++;
+    char destination[] = "t";
+    strcat(destination,temp);
+    printf("%s %s %s %s \n",destination,quad_info->operand1,quad_info->opcode,quad_info->operand2);
+    push(quad_stack,destination);//pushing our destination
+  }
+
 }
 void print_stack(struct Stack *quad_stack)
 {
